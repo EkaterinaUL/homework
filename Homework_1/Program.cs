d6f9c99bb3557a83aaefc18ee51fc2d298144bd4
Ekaterina <katt_217@mail.ru>
2022-11-25 23:26:11 +0800

    Homework semimar1 C#

---
+++ Homework_1/Program.cs
@@ -0,0 +1,72 @@
+﻿// Задача 2
+// Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
+
+//Console.WriteLine("Введите два числа");
+//int num1 = Convert.ToInt32(Console.ReadLine());
+//int num2 = Convert.ToInt32(Console.ReadLine());
+//int max = 0;
+//int min = 0;
+
+//if (num1 < num2)
+//{ 
+   //max = num2;
+   //min = num1;
+//}
+//else
+//{ 
+   //max = num1;
+   //min = num2;
+//}
+//Console.Write("max = ");
+//Console.WriteLine(max);
+//Console.Write("min = ");
+//Console.WriteLine(min);
+
+//Задача 4
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+//Console.WriteLine("Введите три числа");
+//int num1 = Convert.ToInt32(Console.ReadLine());
+//int num2 = Convert.ToInt32(Console.ReadLine());
+//int num3 = Convert.ToInt32(Console.ReadLine());
+//int max = 0;
+
+//if (num1 > num2)
+//{ 
+//   if (num1 > num3) max = num1;
+//}
+//else
+//{ 
+ //  if (num2 > num3) max = num2;
+ //  else max = num3;
+//}
+//Console.Write("max = ");
+//Console.WriteLine(max);
+
+//Задача 6
+//Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+//Console.WriteLine("Введите число");
+//int number = Convert.ToInt32(Console.ReadLine());
+
+//if (number % 2 == 0)
+//{ 
+   //Console.WriteLine("Да");
+//}
+//else
+//{ 
+ //  Console.WriteLine("Нет");
+//}
+
+//Задача 8
+//Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+//Console.WriteLine("Введите число");
+//int n = Convert.ToInt32(Console.ReadLine());
+//int i = 2;
+
+//while (i < n + 1)
+//{
+//    Console.Write($"{i}, ");
+ //   i = i + 2;
+//}
